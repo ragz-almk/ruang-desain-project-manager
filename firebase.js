baa55158555f6daa36d575ee6de9c8a568f66c0e
Raga AL Malik <ragaalmalik@gmail.com>
2026-03-13 12:41:26 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,19 +3,22 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
 // GANTI dengan konfigurasi dari Firebase Console milikmu!
 const firebaseConfig = {
-  apiKey: "API_KEY_KAMU",
-  authDomain: "archimanage-xxx.firebaseapp.com",
-  projectId: "archimanage-xxx",
-  storageBucket: "archimanage-xxx.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyBAYPMg0ijoOIfxZlLrUex_hIBlZaNXk8Q",
+  authDomain: "architecture-manager-490104.firebaseapp.com",
+  projectId: "architecture-manager-490104",
+  storageBucket: "architecture-manager-490104.firebasestorage.app",
+  messagingSenderId: "128091108713",
+  appId: "1:128091108713:web:9a77876d951714a4fd79f8",
+  measurementId: "G-1SZR8BFFCC"
 };
 
 // Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Inisialisasi layanan yang akan kita pakai
 export const auth = getAuth(app);
